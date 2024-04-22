ed53eca1224530fc82b650ff6111d3022ca63d5c
sudeptech <118206782+sudeptech@users.noreply.github.com>
2024-04-21 23:56:40 -0400

    travel application frontend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-container">
+    <MyHeader
+      title="Travel Destinations"
+      subtitle="Embark on a Global Adventure"
+    />
+    <TravelDestinationBox :destinations="destinations" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from "./components/MyHeader.vue";
+import TravelDestinationBox from "./components/TravelDestinationBox.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    MyHeader,
+    TravelDestinationBox,
+  },
+  data() {
+    return {
+      destinations: [],
+    };
+  },
+  methods: {
+    async fetchTravelDestinations() {
+      const res = await fetch("https://sudipthapa.onrender.com/api");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    this.destinations = await this.fetchTravelDestinations();
+  },
+};
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+}
+
+#main-container {
+  padding: 0px 40px;
+  max-width: 1800px;
+  margin: auto;
+}
+
+@media screen and (max-width: 768px) {
+  #main-container {
+    padding: 0px 20px;
+  }
 }
 </style>
